df2ccc0a7c043b861145c0966592e32ce2516d1c
palegreramos <palegre@iespuntadelverde.es>
2022-02-17 13:19:34 +0100

    "final"

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template,request,url_for,redirect
 app = Flask(__name__)
 @app.route("/")
-def home():
-    return "Hola, Flask!"
+@app.route("/<nombre>")
+def home(nombre="Pili"):
+    return f"Hola {nombre}, Flask!"
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        name = request.form['login']
+        return redirect(url_for('home',nombre=name))
+    else:
+        return render_template("login.html")
