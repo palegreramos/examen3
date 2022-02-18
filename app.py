87d55c8440e6f5027841a73f701f8662192ee228
palegreramos <palegre@iespuntadelverde.es>
2022-02-18 09:24:02 +0100

    nueva rama

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask,render_template,request,url_for,redirect
 app = Flask(__name__)
 @app.route("/")
 @app.route("/<nombre>")
-def home(nombre="Pili"):
-    return f"Hola {nombre}, Flask!"
+def home(nombre="invitado"):
+    if nombre=="invitado":
+        return f"Hola {nombre}<br><a href='/login'>Ir a login</a>"
+    else:
+        return f"<h2>Hola {nombre}</h2>"
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
